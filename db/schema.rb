c39a3416ba0b4546c223c05a865f5aaab38d2087
Cezar Halmagean <cezar@mixandgo.com>
2022-05-27 16:01:14 +0300

    Add active flag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_27_060055) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_27_093550) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_27_060055) do
     t.boolean "happy"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active", default: true
   end
 
 end
