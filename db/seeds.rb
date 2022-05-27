8e706a2a85396ec6b12767363454771291634d0b
Cezar Halmagean <cezar@mixandgo.com>
2022-05-27 09:14:37 +0300

    Added users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+10000.times do
+  User.create!(
+    name: Faker::Name.first_name,
+    email: Faker::Internet.email,
+    age: Faker::Number.within(range: 19..80)
+  )
+end
